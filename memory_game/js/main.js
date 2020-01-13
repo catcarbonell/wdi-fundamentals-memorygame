bc21a9c4eeb5343e3c37911ff821b12417726c3a
Cat Carbonell <catcarbonell@gmail.com>
2020-01-12 23:12:18 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 //console.log("Up and running!");
 
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [ 
+  {
+	rank : "queen",
+	suit : "hearts",
+	cardImage : "images/queen-of-hearts.png"
+  },
+  {
+	rank : "queen",
+	suit : "diamonds",
+	cardImage : "images/queen-of-diamonds.png"
+  },
+  {
+	rank : "king",
+	suit : "hearts",
+	cardImage : "images/king-of-hearts.png"
+  },
+  {
+	rank : "king",
+	suit : "diamonds",
+	cardImage : "images/king-of-hearts.png"
+  },
+]
 
 let cardsInPlay = [];
 
@@ -15,9 +36,11 @@ function checkForMatch(){
 
 function flipCard(cardId){
 
-    cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
 
-    console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 
     checkForMatch();
 
