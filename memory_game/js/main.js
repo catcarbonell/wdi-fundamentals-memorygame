758297cd5c647b1644bb4198b215649dbe6acf05
Cat Carbonell <catcarbonell@gmail.com>
2020-01-12 21:39:41 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,22 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+
+const cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+const cardOne = cards[0];
+const cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+} else {
+	alert("Sorry, not a match. Try again!");
+}
